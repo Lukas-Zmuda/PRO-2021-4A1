642d4326d3dc15a54d30d7b69625e1c291d07aae
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2021-10-06 08:06:24 +0200

    teplota

---
+++ Test/src/UlohaTeplota.java
@@ -0,0 +1,26 @@
+
+public class UlohaTeplota {
+
+	public static void main(String[] args) {
+		int celzius = 0;
+		System.out.println(celzius + "\t" + (celzius * 1.8 + 32));
+		celzius += 10;
+		System.out.println(celzius + "\t" + (celzius * 1.8 + 32));
+		celzius += 10;
+		System.out.println(celzius + "\t" + (celzius * 1.8 + 32));
+		celzius += 10;
+		System.out.println(celzius + "\t" + (celzius * 1.8 + 32));
+		celzius += 10;
+		System.out.println(celzius + "\t" + (celzius * 1.8 + 32));
+		celzius += 10;
+		System.out.println(celzius + "\t" + (celzius * 1.8 + 32));
+		celzius += 10;
+		System.out.println(celzius + "\t" + (celzius * 1.8 + 32));
+		celzius += 10;
+		System.out.println(celzius + "\t" + (celzius * 1.8 + 32));
+		celzius += 10;
+		System.out.println(celzius + "\t" + (celzius * 1.8 + 32));
+
+	}
+
+}

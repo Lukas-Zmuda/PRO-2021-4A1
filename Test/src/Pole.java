43013e9930283f1ea44f8d5a4ed8a2ba509a01f5
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-01-13 11:24:23 +0100

    pole ulohy

---
+++ Test/src/Pole.java
@@ -0,0 +1,16 @@
+
+public class Pole {
+
+	public static void main(String[] args) {
+		
+		String[] mena4A1 = {"Tomas", "Michal", "Filip", "Bruno", "Erik"};
+		
+		System.out.println("Pocet ziakov: " + mena4A1.length);
+		System.out.println("Prvy v zozname je " + mena4A1[0] + ".");
+		System.out.println("Posledny v zozname je " + mena4A1[mena4A1.length - 1] + ".");
+		
+		
+
+	}
+
+}
---
+++ Test/src/UPole2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class UPole2 {
+
+	public static void main(String[] args) {
+		//2.
+		// - nacitajte 5 cisel
+		// - vypiste pocet cisel nad priemernou hodotou zadanych cisel
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
